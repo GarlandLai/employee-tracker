dfc40a7843b52a70cdd07a7d2ed5986f28e8b12b
Garland Lai <garlandlai24@gmail.com>
2019-08-10 15:30:54 -0700

    Working on Authentication lessons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root to: 'divisions#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
   resources :divisions do
     resources :employees
-    end
   end
+end
